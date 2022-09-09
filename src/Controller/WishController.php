a1171a182ff634b207f20f5b781130580d459b3a
FERRE24Mo <morgan.ferre85@gmail.com>
2022-09-09 11:51:27 +0200

    add wish list

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -4,8 +4,9 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Entity\Wish;
 
 
 #[Route('/wish', name: 'wish_')]
@@ -17,16 +18,17 @@ class WishController extends AbstractController
     {
         $wishList = $entityManager->getRepository(Wish::class)->findAll();
 
-        // var_dump($wishList);
+         //var_dump($wishList);
         
         return $this->render('wish/list.html.twig', [
             'controller_name' => 'WishController',
             'wish_list' => $wishList
+            
         ]);
     }
 
     #[Route('/{id}', name: 'detail')]
-    public function detail(EntityManagerInterface $entityManager,$id): Response 
+    public function detail($id): Response 
     {
         $wish = $entityManager->getRepository(Wish::class)->find($id);
 
